38371b1dc643199bb1aa94dafce4a0588564c320
Ayush <rishischnabel@gmail.com>
2026-02-13 22:54:16 +0530

    making routes of login , register , logout

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from "cors"
+
+import 'dotenv/config'
+import cookieParser from "cookie-parser"
+import connectDB from './config/db.js'
+import User from './models/User.js'
+import authRoutes from './routes/authRoutes.js'
+const app = express()
+
+const port = process.env.PORT || 4000;
+connectDB()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials : true}))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// API routes
+app.use('/api/auth',authRoutes)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ models/User.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    verifyOtp: {
+      type: String,
+      default: null,
+    },
+
+    verifyOtpExpiry: {
+      type: Date,
+      default: null,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    resetOtp: {
+      type: String,
+      default: null,
+    },
+
+    resetOtpExpiry: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
