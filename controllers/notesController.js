a9655c8e9913034747a4424cba6de3b3a3cf7a7d
Ayush <rishischnabel@gmail.com>
2026-02-17 12:25:53 +0530

    edit

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -54,23 +54,8 @@ export const getNotesbyid = async (req, res) => {
   };
 
 export const updatenotebyid = async (req, res) => {
-    // const { id } = req.params;
-    // const { title, description } = req.body;
-    // try {
-    //   const note = await notes.findById(id);
-    //   if (!note) {
-    //     return res.status(404).json({ message: "Note not found" });
-    //   }
-    //   note.title = title;
-    //   note.description = description;
-    //   await note.save();
-    //   res.status(200).json({ message: "Note updated successfully" });
-    // } catch (error) {
-    //   res.status(500).json({ message: error.message });
-    // }
-    const { id } = req.params;
+  const { id } = req.params;
   const { title, description } = req.body;
-
   try {
     const note = await notes.findById(id);
 
@@ -80,17 +65,13 @@ export const updatenotebyid = async (req, res) => {
         success: false
       });
     }
-
     note.title = title;
     note.description = description;
-
     await note.save();
-
     res.status(200).json({
       message: "Note updated successfully",
       success: true
     });
-
   } catch (error) {
     res.status(500).json({
       message: error.message,
